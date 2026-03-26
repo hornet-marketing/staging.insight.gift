4c0d62d734e8554c0b25528b1c1492c38dfd14a7
hornet-marketing <manuel@hornet.marketing>
2026-03-26 08:56:59 +0100

    chore: Akeeba Backup-Verzeichnis zu .gitignore hinzugefügt

--- administrator/components/com_akeebabackup/backup/akeeba.log.php
+++ administrator/components/com_akeebabackup/backup/akeeba.log.php
@@ -171,3 +171,38 @@ DEBUG   |20251009 19:36:34|-- Loading optional filter Stack\StackErrorlogs
 DEBUG   |20251009 19:36:34|-- Loading optional filter Stack\StackHoststats
 DEBUG   |20251009 19:36:34|-- Loading optional filter Stack\StackActionlogs
 DEBUG   |20251009 19:36:34|-- Loading optional filter Stack\StackFinder
+DEBUG   |20260325 22:21:14| -- Resetting Akeeba Engine factory (backend.id-20260325-222114-953270)
+DEBUG   |20260325 22:21:14|Fetching filter data from database
+DEBUG   |20260325 22:21:14|Loading filters
+DEBUG   |20260325 22:21:14|-- Loading filter Directories
+DEBUG   |20260325 22:21:14|-- Loading filter Extradirs
+DEBUG   |20260325 22:21:14|-- Loading filter Files
+DEBUG   |20260325 22:21:14|-- Loading filter Incremental
+DEBUG   |20260325 22:21:14|-- Loading filter Multidb
+DEBUG   |20260325 22:21:14|-- Loading filter Regexdirectories
+DEBUG   |20260325 22:21:14|-- Loading filter Regexfiles
+DEBUG   |20260325 22:21:14|-- Loading filter Regexskipdirs
+DEBUG   |20260325 22:21:14|-- Loading filter Regexskipfiles
+DEBUG   |20260325 22:21:14|-- Loading filter Regextabledata
+DEBUG   |20260325 22:21:14|-- Loading filter Regextables
+DEBUG   |20260325 22:21:14|-- Loading filter Skipdirs
+DEBUG   |20260325 22:21:14|-- Loading filter Skipfiles
+DEBUG   |20260325 22:21:14|-- Loading filter Tabledata
+DEBUG   |20260325 22:21:14|-- Loading filter Tables
+DEBUG   |20260325 22:21:14|-- Loading filter Tablesalwaysskipped
+DEBUG   |20260325 22:21:14|-- Loading filter Cvsfolders
+DEBUG   |20260325 22:21:14|-- Loading filter Excludefiles
+DEBUG   |20260325 22:21:14|-- Loading filter Excludefolders
+DEBUG   |20260325 22:21:14|-- Loading filter Excludetabledata
+DEBUG   |20260325 22:21:14|-- Loading filter Joomlaskipdirs
+DEBUG   |20260325 22:21:14|-- Loading filter Joomlaskipfiles
+DEBUG   |20260325 22:21:14|-- Loading filter Libraries
+DEBUG   |20260325 22:21:14|-- Loading filter Publicfolder
+DEBUG   |20260325 22:21:14|-- Loading filter Sitedb
+DEBUG   |20260325 22:21:14|-- Loading filter Siteroot
+DEBUG   |20260325 22:21:14|-- Loading filter Systemcachefiles
+DEBUG   |20260325 22:21:14|Loading optional filters
+DEBUG   |20260325 22:21:14|-- Loading optional filter Stack\StackErrorlogs
+DEBUG   |20260325 22:21:14|-- Loading optional filter Stack\StackHoststats
+DEBUG   |20260325 22:21:15|-- Loading optional filter Stack\StackActionlogs
+DEBUG   |20260325 22:21:15|-- Loading optional filter Stack\StackFinder
